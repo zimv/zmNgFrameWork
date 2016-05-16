2d47e0c1762c3976ae7877638a0240b61f64a424
mayiwen <mayiwen@fishsaying.com>
2016-05-16 13:40:19 +0800

    rev-collector update

--- gulp/tasks/rev-collector.js
+++ gulp/tasks/rev-collector.js
@@ -7,19 +7,20 @@ gulp.task('rev-collector', function () {
         .pipe( revCollector({
             replaceReved: true,
             /*
-            	它这替换规则很奇怪，目前这样写并不是替换，而是让它找到build，因为rev-json的目录并不全
+            	rev-json的目录并不全
             	{
 				  "app.js": "app-27923ebf3e.js",
 				  "modules/layout/app.js": "modules/layout/app-aed30707de.js",
 				  "modules/main/app.js": "modules/main/app-57649e17a9.js",
 				  "modules/test/app.js": "modules/test/app-2e3a1ce642.js"
 				}
-				这样的revjson会导致所有app.js被第一个app.js覆盖
-				为什么这样写源于看rev-collector源码，调试试出来的。。
+				这样的revjson不使用dirReplacements会导致所有app.js被第一个app.js覆盖
+				此属性添加后，必须指定的目录才会按照json替换
+				总之这个匹配很奇怪
             */
             dirReplacements:{
-            	"css": "css",
-            	"build": "build"
+            	"css": "css",      //将会匹配含有css的资源路径.进行revjson里的清单替换
+            	"build/": "build/" //将会匹配含有build 再进行revjson替换
             }
         }) )
         .pipe( gulp.dest(config.dest) );//这个输出html
